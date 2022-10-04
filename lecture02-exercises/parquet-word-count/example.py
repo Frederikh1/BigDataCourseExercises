600910165c93337d56c4c38a3e10a00d16eda997
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-10-04 16:12:26 +0200

    parquet working file

--- lecture02-exercises/parquet-word-count/example.py
+++ lecture02-exercises/parquet-word-count/example.py
@@ -1,3 +1,4 @@
+from pydoc import cli
 from hdfs import InsecureClient
 from collections import Counter
 import pyarrow.parquet as pq
@@ -12,5 +13,19 @@ wordcount = None
 with client.read('/alice-in-wonderland.txt', encoding='utf-8') as reader:
     wordcount = Counter(reader.read().split()).most_common(10)
 
-    
+
+print(wordcount)
+df = pd.DataFrame(wordcount)
+table = pa.Table.from_pandas(df)
+pq.write_table(table, "/parquetwordssecond.parquet")
+
+#client.upload("/", "/parquetwordssecond.parquet")
+
+client.download("/parquetwordssecond.parquet", "/downloadedparquet.parquet")
+
+resultTable = pq.read_table("/downloadedparquet.parquet")
+test = resultTable.to_pandas()
+print(test)
+
+
 # To-Do: Save the wordcount in a Parquet file and read it again!
