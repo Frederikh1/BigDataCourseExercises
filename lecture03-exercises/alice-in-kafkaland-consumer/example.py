872033053afd550123b5ac6bb0401bd57f973224
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-10-09 18:32:48 +0200

    yikes

--- lecture03-exercises/alice-in-kafkaland-consumer/example.py
+++ lecture03-exercises/alice-in-kafkaland-consumer/example.py
@@ -3,7 +3,28 @@ from kafka import KafkaConsumer
 
 
 # Create a KafkaConsumer that consumes messages from the topic 'alice-in-kafkaland'
+consumer = KafkaConsumer('alice2', bootstrap_servers=['kafka:9092'], group_id='group1',enable_auto_commit=True)
+consumer.poll()
+consumer.seek_to_end()
 
 # Combine all the messages into a single string
+fileContents = ''
 
+for msg in consumer:
+    eventValue = msg.value.decode('utf-8')
+    if(eventValue == 'FINISHED'):
+        break
+    fileContents += eventValue
+    fileContents += "\n"
+consumer.close()
+print(fileContents)
+    
+    
 # Write the string to HDFS in a file called 'alice-in-kafkaland.txt'
+client = InsecureClient('http://namenode:9870', user='root')
+with client.write('/alice-in-kafkaland2.txt', encoding='utf-8', overwrite=True) as writer:
+    writer.write(fileContents)
+    writer.flush()
+  
+# Reading a file, using a delimiter makes it return a list
+
--- lecture03-exercises/alice-in-kafkaland-producer/example.py
+++ lecture03-exercises/alice-in-kafkaland-producer/example.py
@@ -1,8 +1,20 @@
+from math import prod
 from hdfs import InsecureClient
 from kafka import KafkaProducer
 
 # Create an insecure client that can read from HDFS
+client = InsecureClient('http://namenode:9870', user='root')
 
 # Read the alice in wonderland text file from HDFS
 
+
+# Reading a file, using a delimiter makes it return a list
+with client.read('/alice-in-wonderland.txt', encoding='utf-8', delimiter='\n') as reader:
 # Write each sentence in alice in wonderland to a kafka topic with a KafkaProducer
+    producer = KafkaProducer(bootstrap_servers='kafka:9092')
+    for line in reader:
+        serialized = bytes(line, 'utf-8')
+        producer.send('alice2', serialized)
+    producer.send('alice2', bytes('FINISHED', 'utf-8'))
+    producer.flush()
+
